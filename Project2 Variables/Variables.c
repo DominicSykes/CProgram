e22837bf0a30bea54a70810b75f92bfbe34db5fd
DomSykes <65317380+DominicSykes@users.noreply.github.com>
2020-08-02 15:55:08 -0500

    Add files via upload

--- Project2 Variables/Variables.c
+++ Project2 Variables/Variables.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	char characterName[] = "Dominic Sykes";
+	char characterName[] = "Mr. Sykes";
 	printf("Hello World! \nI am %s. \n", characterName);
-	printf("I am 24 years old with a diploma in Computer Systems Technology. \n");
+	printf("I am 24 years old with a certification in Web Design and a diploma in Computer Systems Technology. \n");
+	printf("I am dyslexic. I have went to The Gow School, graduated, went to St. Mary's University, then went to go to \n");
+	printf("Vector Technology Institute in Jamaica, where I got my first diploma in computer systems technology in 2019. \n \n \n \n");
+	printf("I am at Durham College getting my Computer Systems Technician Diploma!");
+	//Go Onlinegdp.com and copy and replace the code there with the one on the bottom, select the c program in the upper right then run it.
 	return 0;
 }
